4a8a67d1ec462ca8f3ec739c82048cf83c08f5e8
muratzorlu <murtipan@gmail.com>
2020-09-09 11:56:46 +0300

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {action: "SendIt"}, function(response) {});  
+});
+
+  }
+})
